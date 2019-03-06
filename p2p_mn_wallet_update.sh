a1107bbe1a2336e0576e80a6b9234c7fa81862b3
p2p-coins.pro <support@p2p-coins.pro>
2019-03-06 13:11:42 +0200

    Create p2p_mn_wallet_update.sh

---
+++ p2p_mn_wallet_update.sh
@@ -0,0 +1,52 @@
+COIN_TGZ='https://github.com/p2pcoinspro/p2p-master/releases/download/v1.0.1.5/p2pcoin-v1.0.1.5-linux-x32x64.tar.xz'
+
+TMP_FOLDER=$(mktemp -d)
+CONFIG_FILE='p2p.conf'
+CONFIGFOLDER='/root/.p2p'
+COIN_DAEMON='p2pd'
+COIN_CLI='p2p-cli'
+COIN_PATH='/usr/local/bin/'
+COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
+COIN_NAME='p2p'
+COIN_PORT=24513
+RPC_PORT=24514
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+function download_node() {
+  echo -e "Prepare to download ${GREEN}$COIN_NAME${NC}."
+  cd $TMP_FOLDER >/dev/null 2>&1
+  wget -q $COIN_TGZ
+  compile_error
+  tar -xvf $COIN_ZIP >/dev/null 2>&1
+  compile_error
+  cp p2pcoin*/* $COIN_PATH
+  cd - >/dev/null 2>&1
+  rm -rf $TMP_FOLDER >/dev/null 2>&1
+  chmod +x $COIN_PATH$COIN_DAEMON $COIN_PATH$COIN_CLI
+  clear
+}
+
+function compile_error() {
+if [ "$?" -gt "0" ];
+ then
+  echo -e "${RED}Failed to compile $COIN_NAME. Please investigate.${NC}"
+  exit 1
+fi
+}
+
+
+function wallet_restart() {
+  systemctl stop $COIN_NAME.service
+  sleep 10
+  killall -9 $COIN_DAEMON
+  systemctl start $COIN_NAME.service
+}
+
+
+download_node
+wallet_restart
+
+
