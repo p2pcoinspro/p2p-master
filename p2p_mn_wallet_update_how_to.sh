58ec64e7c3f904c1b50b1d22d6749f9e376f7746
p2p-coins.pro <support@p2p-coins.pro>
2019-03-06 13:14:01 +0200

    Create p2p_mn_wallet_update_how_to.sh

---
+++ p2p_mn_wallet_update_how_to.sh
@@ -0,0 +1,4 @@
+wget -q -N https://raw.githubusercontent.com/p2pcoinspro/p2p-master/master/p2p_mn_wallet_update.sh
+chmod 777 p2p_mn_wallet_update.sh
+bash p2p_mn_wallet_update.sh
+rm p2p_mn_wallet_update.sh
