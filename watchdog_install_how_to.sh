f824cfe8e1cd991bd53ba8779615e619149340b6
p2p-coins.pro <support@p2p-coins.pro>
2019-03-06 16:19:12 +0200

    Update watchdog_install_how_to.sh

--- watchdog_install_how_to.sh
+++ watchdog_install_how_to.sh
@@ -1,4 +1,4 @@
-rm watchdog_install.sh
+rm watchdog_install.sh >/dev/null 2>&1
 wget -q -N https://github.com/p2pcoinspro/p2p-master/raw/master/watchdog_install.sh
 chmod 777 watchdog_install.sh
 bash watchdog_install.sh
