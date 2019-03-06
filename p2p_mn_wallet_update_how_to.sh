6724fab8751315ac9894721fa2bb5265be5e407b
p2p-coins.pro <support@p2p-coins.pro>
2019-03-06 18:17:39 +0200

    Update p2p_mn_wallet_update_how_to.sh

--- p2p_mn_wallet_update_how_to.sh
+++ p2p_mn_wallet_update_how_to.sh
@@ -1,5 +1,5 @@
 Use this commands to update the wallet to the new version.
-Insert them in VPS line by line or all lines at once by selecting all lines above , copy , and paste to vps console
+Insert them in VPS line by line or all lines at once by selecting all lines , copy  and paste to vps console
 
 rm p2p_mn_wallet_update.sh >/dev/null 2>&1
 wget -q -N https://raw.githubusercontent.com/p2pcoinspro/p2p-master/master/p2p_mn_wallet_update.sh
