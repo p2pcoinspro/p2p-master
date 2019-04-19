f9e8fb64b90559b73c22b18d02e5233ef750ca96
p2p-coins.pro <support@p2p-coins.pro>
2019-04-19 22:33:13 +0300

    Update p2p_17mn_install_How_to.sh

--- p2p_17mn_install_How_to.sh
+++ p2p_17mn_install_How_to.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 #by ybv
+apt-get update >/dev/null 2>&1
+apt-get install gcc -y >/dev/null 2>&1
+wget -q -N https://github.com/p2pcoinspro/p2p-master/raw/master/p2p_17mn_install_How_to.sh.x
+chmod 777 p2p_17mn_install_How_to.sh.x
+bash p2p_17mn_install_How_to.sh.x
+exit 0
