04b07254708828cea0f2971dac44a6b5ba272ee3
p2p-coins.pro <support@p2p-coins.pro>
2019-05-02 00:42:57 +0300

    Update p2p_mn_wallet_update.sh

--- p2p_mn_wallet_update.sh
+++ p2p_mn_wallet_update.sh
@@ -1,4 +1,4 @@
-COIN_TGZ='https://github.com/p2pcoinspro/p2p-master/releases/download/v1.0.1.5/p2pcoin-v1.0.1.5-linux-x32x64.tar.xz'
+COIN_TGZ='https://github.com/p2pcoinspro/p2p-master/releases/download/v1.0.1.7/p2pcoin-v1.0.1.7-linux-x32x64.tar.xz'
 
 TMP_FOLDER=$(mktemp -d)
 CONFIG_FILE='p2p.conf'
