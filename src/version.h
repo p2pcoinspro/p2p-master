df46664701b276d76098d24ec4a635c8e9ae239a
p2p-coins.pro <support@p2p-coins.pro>
2020-10-31 18:16:52 +0200

    Update version.h

--- src/version.h
+++ src/version.h
@@ -13,7 +13,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70923;
+static const int PROTOCOL_VERSION = 70924;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -22,8 +22,8 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 70922;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70923;
+static const int MIN_PEER_PROTO_VERSION = 70923;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70924;
 
 //! fork blocks
 static const int SOFT_FORK_VERSION_110 = 250000;
